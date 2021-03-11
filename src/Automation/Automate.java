6affc7cf90300a789e109745c499d250c7f532a7
qalamsal <56513960+qalamsal@users.noreply.github.com>
2021-03-11 17:41:01 -0600

    Update Automate.java

--- src/Automation/Automate.java
+++ src/Automation/Automate.java
@@ -2,6 +2,6 @@ package Automation;
 
 public class Automate {
     public static void main(String[] args) {
-        System.out.println("Automation");
+        System.out.println("Automation !!!");
     }
 }
