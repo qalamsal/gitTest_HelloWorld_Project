8f447e18eb5bf911364745c0d9775045d34f0c89
qalamsal <56513960+qalamsal@users.noreply.github.com>
2021-03-12 14:27:25 -0600

    Update Automate.java
    added two variables

--- src/Automation/Automate.java
+++ src/Automation/Automate.java
@@ -3,5 +3,8 @@ package Automation;
 public class Automate {
     public static void main(String[] args) {
         System.out.println("Automation !!!");
+        int a=10;
+        int b=10;
+        System.out.println(a+b);
     }
 }
